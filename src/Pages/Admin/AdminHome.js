c9cc31ed01fb958092718a13d6e5396512e6d51a
Adi <96688318+AdiAkhileshSingh15@users.noreply.github.com>
2023-06-17 02:31:39 +0530

    changing navigate use

--- src/Pages/Admin/AdminHome.js
+++ src/Pages/Admin/AdminHome.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom'
 import AdminHomeHelper from '../../Components/AdminHomeHelper'
 import Footer from '../../Components/Footer';
 
 const AdminHome = () => {
-    const state = useSelector((store) => store.admin)
+    const { admin, isAuthenticated } = useSelector((store) => store.admin)
     const navigate = useNavigate()
+    useEffect(() => {
+        if (!isAuthenticated) {
+            navigate('/adminLogin')
+        }
+    })
     return (
         <div>
 
-            {state.isAuthenticated ? <>
+            {isAuthenticated ? <>
                 <AdminHomeHelper />
                 <div className="container">
                     <div className="row mt-5">
@@ -20,10 +25,10 @@ const AdminHome = () => {
                             <div className="row">
                                 <div className="col-md-5">
                                     <div className="card" style={{ width: "18rem" }}>
-                                        <img className="card-img-top" src={state.admin.avatar} alt="Card image cap" />
+                                        <img className="card-img-top" src={admin.avatar} alt="Card cap" />
                                         <div className="card-body">
-                                            <h5 className="card-title">{state.admin.name}</h5>
-                                            <h5 className="card-title">{state.admin.registrationNumber}</h5>
+                                            <h5 className="card-title">{admin.name}</h5>
+                                            <h5 className="card-title">{admin.registrationNumber}</h5>
                                         </div>
                                     </div>
                                 </div>
@@ -32,28 +37,28 @@ const AdminHome = () => {
                                         <tbody>
                                             <tr>
                                                 <td>Name</td>
-                                                <td>{state.admin.name}</td>
+                                                <td>{admin.name}</td>
                                             </tr>
                                             <tr>
                                                 <td>Email</td>
-                                                <td>{state.admin.email}</td>
+                                                <td>{admin.email}</td>
                                             </tr>
                                             <tr>
                                                 <td>Registration Number</td>
-                                                <td>{state.admin.registrationNumber}</td>
+                                                <td>{admin.registrationNumber}</td>
                                             </tr>
                                             <tr>
                                                 <td>Joining Year</td>
-                                                <td>{state.admin.joiningYear}</td>
+                                                <td>{admin.joiningYear}</td>
                                             </tr>
                                             <tr>
                                                 <td>Department</td>
-                                                <td>{state.admin.department}</td>
+                                                <td>{admin.department}</td>
                                             </tr>
                                             <tr>
                                                 <td>Contact Number</td>
-                                                <td>{state.admin.contactNumber ?
-                                                    state.admin.contactNumber : "NA"}</td>
+                                                <td>{admin.contactNumber ?
+                                                    admin.contactNumber : "NA"}</td>
                                             </tr>
                                         </tbody>
                                     </table>
