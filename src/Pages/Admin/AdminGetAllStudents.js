38afe1d5795358aaca88b35a449cbf287f23b5e1
Adi <96688318+AdiAkhileshSingh15@users.noreply.github.com>
2023-06-17 04:38:06 +0530

    Minor Fixes

--- src/Pages/Admin/AdminGetAllStudents.js
+++ src/Pages/Admin/AdminGetAllStudents.js
@@ -33,6 +33,8 @@ const AdminGetAllStudent = () => {
         if (!response.ok) {
             setError(json)
             setIsLoading(false)
+            setDepartment('')
+            setYear('')
         }
         if (response.ok) {
             setError({})
